1ecc30c89fd45e84d6fd9448c3cd3c4617031c43
kadp <kadp@yandex.ru>
2020-10-12 03:13:07 +0300

    link /:id - err

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 import PropTypes from "prop-types";
 import Main from "../main/main";
+import SingIn from "../sing-in/sing-in";
+import MyList from "../my-list/my-list";
+import Film from "../film/film";
+import AddReview from "../add-review/add-review";
+import Player from "../player/player";
 
 
 const App = (props) => {
@@ -8,7 +14,22 @@ const App = (props) => {
   const {genre, releaseDate} = props;
 
   return (
-    <Main genre={genre} releaseDate={releaseDate} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main genre={genre} releaseDate={releaseDate} />
+        </Route>
+        <Route exact path="/login">
+          <SingIn />
+        </Route>
+        <Route exact path="/mylist">
+          <MyList />
+        </Route>
+        <Route exact path="/films/:id?" component={Film} />
+        <Route exact path="/films/:id?/review" component={AddReview} />
+        <Route exact path="/player/:id?" component={Player} />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
